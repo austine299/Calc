c975caebd27131a0776b44489987f647125b83ca
ugochukwu111 <ugochukwuaugustine@gmail.com>
2020-11-12 00:39:41 +0100

    calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  state = {
+    count :''
+  }
+
+  Handle=(digit)=>{
+    const{count}=this.state
+    this.setState({
+      count:count==='0'?String(digit):count+digit
+    })
+  }
+
+  addDot=()=>{
+    const {count}= this.state
+    if (count.indexOf('.')===-1){
+      this.setState({
+        count:count+'.'
+      })
+    }
+  }
+
+  reset=()=>{
+    const {count}= this.state
+    this.setState({
+      count:''
+    })
+  }
+
+  Calculate=()=>{
+    const{count}=this.state
+    this.setState({
+      count:eval(count)
+    })
+  }
+
+
+
+  render(){
+
+    return (
+      <div className="container">
+        <div className="calcbody">
+          <div className="input">
+            <input value={this.state.count}/>
+            <h1>{this.state.count}</h1>
+          </div>
+          <div className="btnbgk">
+            <div className="btnposition">
+              <button className="resetbtn" onClick={()=>this.reset()}>C</button>
+              <button onClick={()=>this.Handle('()')}>()</button>
+              <button onClick={()=>this.Handle('%')}>%</button>
+              <button onClick={()=>this.Handle('/')}>/</button>
+              <button onClick={()=>this.Handle(7)}>7</button>
+              <button onClick={()=>this.Handle(8)}>8</button>
+              <button onClick={()=>this.Handle(9)}>9</button>
+                <button onClick={()=>this.Handle('*')}>*</button>
+              <button onClick={()=>this.Handle(4)}>4</button>
+              <button onClick={()=>this.Handle(5)}>5</button>
+              <button onClick={()=>this.Handle(6)}>6</button>
+              <button onClick={()=>this.Handle('-')}>-</button>
+              <button onClick={()=>this.Handle(1)}>1</button>
+              <button onClick={()=>this.Handle(2)}>2</button>
+              <button onClick={()=>this.Handle(3)}>3</button>
+              <button onClick={()=>this.Handle('+')}>+</button>
+              <button onClick={()=>this.Handle('+/-')}>+/-</button>
+              <button onClick={()=>this.Handle(0)}>0</button>
+              <button onClick={()=>this.addDot()}>.</button>
+              <button className="equebtn" onClick={()=>this.Calculate()}>=</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
